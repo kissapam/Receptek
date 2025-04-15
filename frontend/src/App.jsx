dc07607353668e476d8fcad1e15966ea05ba5436
kissapam <kissrobilaci@gmail.com>
2025-04-15 17:17:20 +0200

    masodik_UseState és Effect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [receptek, setReceptek] = useState([])
+
+
+useEffect(() => {         // hogy egyszer fusson le
+  fetch("http://127.0.0.1:8000/recipes/")
+  .then( (valasz) => { return valasz.json() })
+  .then( (receptek) => { setReceptek(receptek) })
+}, [] )                   // üress a tömb, ezért fut le egyszer. Nincs beállítva figyelés
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      {receptek.map(recept => <div>{recept.name}</div>)}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
